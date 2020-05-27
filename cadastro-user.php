7bdb6a3185054b069012acbe20bd5d7d71e8e7e4
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-05-27 00:44:38 -0300

    adicionando página de cadastro

--- cadastro-user.php
+++ cadastro-user.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/form.css">
     <title>Cadastro | Samplaces</title>
 </head>
 <body>
@@ -20,38 +21,63 @@
     </header>
 
     <main>
-        <h3>Cadastro</h3>
+        <h3>cadastro</h3>
 
         <form method="POST" class="cad-user">
 
             <div id="nome">
-            <label for="nomeUsuario">Nome Completo</label><br>
-                <input type="text" name="nomeUsuario" id="nomeUsuario" value="<?= $nome ?>" placeholder="Maria da Silva" required>
-                <?= ($nomeOk? '': '<span class="erro">O nome é muito curto') ?>
+            <label for="nome">nome completo</label><br>
+                <input type="text" name="nome" id="nome" value="<?= $nome ?>" placeholder="Maria da Silva" required><br>
+                <!-- <?= ($nomeOk? '': '<span class="erro">O nome é muito curto') ?> -->
+            </div>
+
+            <div id="user">
+            <label for="user">user</label><br>
+                <input type="text" name="user" id="user" value="<?= $nome ?>" placeholder="maria" required><br>
+                <!-- <?= ($nomeOk? '': '<span class="erro">O nome é muito curto') ?> -->
+            </div>
+
+            <div id="bairro">
+            <label for="bairro">bairro</label><br>
+                <input type="text" name="bairro" id="bairro" value="<?= $nome ?>" placeholder="República"><br>
+                <!-- <?= ($nomeOk? '': '<span class="erro">O nome é muito curto') ?><br> -->
+                <input class="checkmark" type="checkbox" name="naosp" id="naosp" value="naosp">
+                <label for="naosp">não moro em São Paulo</label>
             </div>
 
             <div id="email">
-            <label for="emailUsuario">E-mail</label><br>
-            <input type="email" name="emailUsuario" id="emailUsuario" value="<?= $email ?>" placeholder="maria@email.com" required><br>
-            <?= ($emailOk? '': '<span class="erro">O e-mail é inválido') ?>
+            <label for="email">e-mail</label><br>
+            <input type="email" name="email" id="email" value="<?= $email ?>" placeholder="maria@email.com" required><br><br>
+            <!-- <?= ($emailOk? '': '<span class="erro">O e-mail é inválido') ?> -->
             </div>
 
-            <div id ="senhas">
-            <div id="senha">
-            <label for="senhaUsuario">Senha</label><br>
-                <input type="password" name="senhaUsuario" id="senhaUsuario" value="<?= $senha ?>" required><br>
-                <?= ($senhaOk? '': '<span class="erro">A senha é muito curta') ?><br>
-                <?= ($confirmacaoOk? '': '<span class="erro">A senha e a confirmação são diferentes') ?>
+            <div id="senhas">
+                <div id="senha">
+                <label for="senha">senha</label><br>
+                    <input type="password" name="senha" id="senha" value="<?= $senha ?>" required><br>
+                    <!-- <?= ($senhaOk? '': '<span class="erro">A senha é muito curta') ?><br>
+                    <?= ($confirmacaoOk? '': '<span class="erro">A senha e a confirmação são diferentes') ?> -->
+                </div>
+
+                <div id="confirmacao">
+                <label for="confirmacao">confirmação de senha</label><br>
+                    <input type="password" name="confirmacao" id="confirmacao" value="<?= $confirmacao ?>" required><br>
+                </div>
             </div>
 
-            <div id="confirmacao">
-            <label for="confirmacaoUsuario">Confirmação de Senha</label><br>
-                <input type="password" name="confirmacaoUsuario" id="confirmacaoUsuario" value="<?= $confirmacao ?>" required><br>
-            </div>
-            </div>
+            <div class="foto-user">
+                    <label for="foto">selecione a sua foto de perfil</label><br>
+                    <p>de preferência uma foto quadradinha</p>
+                    <label class="select-foto">
+                        <input type="file" name="foto" id="foto" accept=".jpg,.jpeg,.png,.gif"><br>
 
-            <div>
-            <button type="submit">Enviar</button>
+                        <img src="img/profile.png" id="foto-carregada"><br>
+                    </label>
+                    <!-- <?= ($imagemOk? '' : '<span class="erro">A imagem é inválida')?><br> -->
+                </div>
+
+            <div class="botao">
+            <button class="botao-grande" type="submit">Enviar</button>
             </div>
         </form>
         
@@ -70,5 +96,16 @@
             <img src="img/github.png" alt="GitHub">
         </a>     
     </footer>
+
+
+    <script>
+        document.getElementById("foto").onchange = (evt) => {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                document.getElementById("foto-carregada").src = e.target.result;
+            };
+            reader.readAsDataURL(evt.target.files[0]);
+        };
+    </script>
 </body>
 </html>
